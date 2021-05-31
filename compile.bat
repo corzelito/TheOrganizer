9a9b526160906da7e9360d7c3e6fb6775c5bf8bd
Corzelito <corzelito@gmail.com>
2021-05-31 11:20:47 +0200

    Version 1.0!

---
+++ compile.bat
@@ -0,0 +1,1 @@
+pyinstaller --noconfirm --onefile --windowed --icon "D:/Proyectos/TheOrganizer/ui/app_icon.ico" --name "TheOrganizer" --add-data "D:/Proyectos/TheOrganizer/locales;locales/" --add-data "D:/Proyectos/TheOrganizer/ui;ui/"  "D:/Proyectos/TheOrganizer/main.py"
